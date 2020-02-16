fbec2f7e136ce1d89bd6a06d66b4db83758d8f6f
EC2 Default User <ec2-user@ip-172-31-38-56.us-east-2.compute.internal>
2020-02-16 22:39:30 +0000

    part 2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,9 +8,11 @@ class MoviesController < ApplicationController
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
+    
   end
 
   def index
+    @all_ratings = Movie.ratings
     @sort = params[:sort]
     @movies = Movie.all.order(@sort)
   end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ActiveRecord::Base
+    def self.ratings
+        Movie.select(:raing).distinct.injet([]) {|a, m| a.push m.rating} 
+    end
 end
